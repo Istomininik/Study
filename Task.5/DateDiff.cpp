0117eafc683c7653a964a3082374455d47c51829
Василий Истомин <vasily.d.istomin@gmail.com>
2021-10-07 14:17:37 +0300

    Создан класс Date и созданы методы по заданию

---
+++ Task.5/DateDiff.cpp
@@ -0,0 +1,105 @@
+﻿#include "DateDiff.h"
+#include <sstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+Date::Date()
+{
+	day = 01;
+	month = 01;
+	year = 1900;
+}
+
+Date::Date(int d, int m, int y)
+{
+	day = d;
+	month = m;
+	year = y;
+}
+
+std::string Date::toString() const
+{
+    stringstream ss;
+    ss << day << ". " << month << ". " << year;
+    string str = ss.str();
+    return string(str);
+}
+
+int days_in_month[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+int Date::Dif(const Date& other)
+{
+	int x, y, s, p, k;
+	x = month;
+	y = other.month;
+	s = 0;
+	p = 0;
+	x = x - 1;
+	while (x > 0)
+	{
+		s = s + days_in_month[x-1];
+		x--;
+	}
+	s = s + day + year * 365;
+	y = y - 1;
+	while (y > 0)
+	{
+		p = p + days_in_month[y-1];
+		y--;
+	}
+	p = p + other.day + other.year * 365;
+	k = s - p;
+	if (k < 0)
+	{
+		k = abs(k);
+	}
+	return k;
+}
+
+int Date::getDay()
+{
+    return day;
+}
+
+int Date::getMonth()
+{
+    return month;
+}
+
+int Date::getYear()
+{
+    return year;
+}
+
+
+void Date::setDay(int d, Date& date)
+{
+	day = d;
+}
+
+void Date::setMonth(int m, Date& date) throw(std::invalid_argument)
+{
+	if (m >= 1 && m <= 12)
+	{
+		month = m;
+	}
+	else
+		throw(invalid_argument("The month is NOT correct!\n"));
+	int x = days_in_month[month-1];
+	if (date.getDay() < 1 || date.getDay() > x)
+	{
+		throw(invalid_argument("The day is NOT correct!\n"));
+	}
+}
+
+void Date::setYear(int y) throw(std::invalid_argument)
+{
+	if (y >= 0)
+	{
+		year = y;
+	}
+	else
+		throw(invalid_argument("The year is NOT correct!\n"));
+}
---
+++ Task.5/DateDiff.h
@@ -0,0 +1,96 @@
+﻿#pragma once
+#include <string>
+
+/* В данном решении поставленной задачи все годы имеют одинаковое количество дней.
+* Т.е. во всех годах 365 дней. */
+
+/**
+ * @brief Класс дата.
+*/
+class Date
+{
+private:
+
+	/**
+	 * @brief День.
+	*/
+	int day;
+
+	/**
+	 * @brief Месяц.
+	*/
+	int month;
+
+	/**
+	 * @brief Год.
+	*/
+	int year;
+
+public:
+	
+	/**
+	 * @brief Конструктор по умолчанию.
+	*/
+	Date();
+
+	/**
+	 * @brief Параметризованный кончтруктор.
+	 * @param d День.
+	 * @param m Месяц.
+	 * @param y Год.
+	*/
+	Date(int d, int m, int y);
+
+	/**
+	 * @brief Метод вывода даты в виде строки.
+	 * @return Дата.
+	*/
+	std::string toString() const;
+
+	/**
+	 * @brief Метод, вычисляющий разницу в днях между датами.
+	 * @param other Вторая дата.
+	 * @return Количесво дней.
+	*/
+	int Dif(const Date& other);
+
+
+	/**
+	 * @brief Метод, возвращающий день.
+	 * @return День.
+	*/
+	int getDay();
+
+	/**
+	 * @brief Метод, возвращяющий месяц.
+	 * @return Месяц.
+	*/
+	int getMonth();
+
+	/**
+	 * @brief Метод, возвращающий год.
+	 * @return Год.
+	*/
+	int getYear();
+
+
+	/**
+	 * @brief Метод изменения дня.
+	 * @param d День, на который изменить.
+	 * @param date Вся дата.
+	*/
+	void setDay(int d,Date& date);
+
+	/**
+	 * @brief Метод изменения месяца.
+	 * @param m Месяц, на который изменить.
+	 * @param date Вся дата.
+	*/
+	void setMonth(int m, Date& date);
+
+	/**
+	 * @brief Метод для изменения года.
+	 * @param y Год, на который изменить.
+	*/
+	void setYear(int y);
+};
